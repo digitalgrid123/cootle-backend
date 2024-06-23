4cbe766fd74cf24dd828da68473bae027db40d31
Yasir Maqsood <yasiranasbro@gmail.com>
2024-06-23 14:51:24 +0500

    made changes for admin

--- cootle/portal/forms.py
+++ cootle/portal/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Purpose, Mapping
+from .models import Purpose, Mapping, ProjectEffort
 
 class PurposeAdminForm(forms.ModelForm):
     class Meta:
@@ -9,3 +9,12 @@ class PurposeAdminForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(PurposeAdminForm, self).__init__(*args, **kwargs)
         self.fields['desired_outcomes'].queryset = Mapping.objects.filter(type='OUT')
+
+class ProjectEffortAdminForm(forms.ModelForm):
+    class Meta:
+        model = ProjectEffort
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectEffortAdminForm, self).__init__(*args, **kwargs)
+        self.fields['outcome'].queryset = Mapping.objects.filter(type='OUT')
